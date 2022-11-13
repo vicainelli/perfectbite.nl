9485ad60f92de0018966fb598b5d27b7bd291e9b
Vinicius Cainelli <vinicius.cainelli@ingka.ikea.com>
2022-11-13 12:12:56 +0100

    init astro.build

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextTranslate = require('next-translate')
-
-const nextConfig = {
-  ...nextTranslate(),
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
