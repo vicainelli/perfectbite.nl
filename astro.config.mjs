6f40ae7ff9d566d4cd41018517f9200f70ac2cec
Vinicius Cainelli <vinicius.cainelli@ingka.ikea.com>
2022-11-13 18:34:14 +0100

    react / dark mode / product page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://www.perfectbite.nl/',
+  integrations: [tailwind(), react()]
 });
