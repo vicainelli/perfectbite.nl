e3665ffecc828558b38df1cc5f3fe795f0319728
Vinicius Cainelli <vinicius.cainelli@ingka.ikea.com>
2024-12-24 18:52:18 +0100

    feat: upgrade to astro 5 (#5)
    * feat: clean up
    
    * feat: add tailwindcss
    
    * docs: update readme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+
 import tailwind from '@astrojs/tailwind';
-import react from "@astrojs/react";
-import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.perfectbite.nl/',
-  integrations: [tailwind(), react(), astroI18next()]
+  integrations: [tailwind()]
 });
